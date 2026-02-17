869ee5e7620fffa59ca25bee2a40f5fe57646f93
Niru-26016 <niru26016@gmail.com>
2026-02-17 19:13:18 +0530

    added bottom tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,27 @@
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-
 export default function TabsLayout() {
   return (
     <>
       <Tabs>
-        <FontAwesome name="home" size={24} color="black" />
-        <Tabs.Screen name="index" options={{ title: "home", tabBarIcon: ()=>(
-          <FontAwesome name="home" size={24} color="black" />
-        ), }} />
-        <Tabs.Screen name="login" options={{ title: "Login" }} />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="home" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="login"
+          options={{
+            title: "Login",
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="user" size={24} color={color} />
+            ),
+          }}
+        />
       </Tabs>
     </>
   );
