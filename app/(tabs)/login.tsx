545a52c769e22fef245176a82bc9c5cf1423a0a3
Niru-26016 <niru26016@gmail.com>
2026-02-17 19:06:23 +0530

    initial commit

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,11 @@
+import {View,Text} from "react-native"
+
+
+export default function loginScreen(){
+    <View>
+        <Text>
+            Hello this is login page
+        </Text>
+
+    </View>
+}
